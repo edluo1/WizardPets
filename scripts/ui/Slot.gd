cff288c677af4a74da5072037f72a70249e5b942
EdX7 <edluo1@bu.edu>
2023-11-08 01:57:25 -0800

    Added some more inventory-related code
    lot of it based on the code in https://www.youtube.com/watch?v=FHYb63ppHmk and improved to Godot 4

---
+++ scripts/ui/Slot.gd
@@ -0,0 +1,29 @@
+extends Panel
+
+var ItemClass = preload("res://scenes/ui/Item.tscn")
+var item = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if randi() % 2 == 0:
+		item = ItemClass.instantiate()
+		add_child(item)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func pickFromSlot():
+	if item != null:
+		remove_child(item)
+		var inventoryNode = find_parent("Inventory")
+		inventoryNode.add_child(item)
+		item = null
+
+func putIntoSlot(new_item):
+	if new_item != null:
+		item = new_item
+		item.position = Vector2(0, 0)
+		var inventoryNode = find_parent("Inventory")
+		inventoryNode.remove_child(item)
+		add_child(item)

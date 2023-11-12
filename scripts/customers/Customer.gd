bf84415a8b6fab39136ca2028f71c867edd9f1c5
EdX7 <edluo1@bu.edu>
2023-11-12 02:29:36 -0800

    Got the dialogue working on a scene

--- scripts/customers/Customer.gd
+++ scripts/customers/Customer.gd
@@ -8,11 +8,14 @@ var want_types = [
 	{"type": "catbeast", "health": 112}
 ]
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var at_order_counter = true
 
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	if at_order_counter:
+		DialogueManager.show_example_dialogue_balloon(load("res://dialogue/test.dialogue"), "start")
+		at_order_counter = false
